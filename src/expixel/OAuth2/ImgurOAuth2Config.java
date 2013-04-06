edd1f9c8072187aa6758cee51ba754c105060103
Adolph C <dolphyO.o@gmail.com>
2013-04-05 23:23:38 -0400

    Added way to store OAuth info.
    Signed-off-by: Adolph C <dolphyO.o@gmail.com>

--- src/expixel/OAuth2/ImgurOAuth2Config.java
+++
@@ -1,6 +0,0 @@
-package expixel.OAuth2;
-
-public class ImgurOAuth2Config {
-	public static final String clientSecret  = "[Client Secret Test]";
-	public static final String clientID = "[Client ID]";
-}
